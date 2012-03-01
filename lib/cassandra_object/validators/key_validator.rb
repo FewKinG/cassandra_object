715e5a9585431895491ccec9959f224f03f11e83
Sven C <fewking@paniccrew.de>
2012-03-01 15:38:42 +0100

    validation message

--- lib/cassandra_object/validators/key_validator.rb
+++ lib/cassandra_object/validators/key_validator.rb
@@ -6,9 +6,9 @@ module CassandraObject
 
       def validate_each(record, attribute, value)
 	if value.to_s.blank?
-	  record.errors.add attribute, (options[:message] || "can't be blank")
+	  record.errors.add attribute, (options[:message] || I18n.t("activemodel.errors.models.#{record.class.to_s.downcase}.attributes.key.blank"))
 	else
-	  record.errors.add attribute, (options[:message] || "has already been taken") if record.new_record? and record.class.connection.exists?(record.class.column_family, value.to_s)
+	  record.errors.add attribute, (options[:message] || I18n.t("activemodel.errors.models.#{record.class.to_s.downcase}.attributes.key.taken")) if record.new_record? and record.class.connection.exists?(record.class.column_family, value.to_s)
 	end
       end
 
